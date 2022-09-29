0644cbbcb2cab401ef06c56c797932faf7fbbe10
Swaron <swaronline0@gmail.com>
2022-09-29 16:29:56 +0300

    Updates

---
+++ precache-manifest.4a3adbf6c2cc4bf77bc179fca85fa30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f8cb1880235c701ac04f89ea6f83d7",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "f627dc1a67943a388e4c",
+    "url": "/counter/static/css/main.e5d1b711.chunk.css"
+  },
+  {
+    "revision": "1992dee0d1af8dbdeab6",
+    "url": "/counter/static/js/2.321b5fda.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/counter/static/js/2.321b5fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f627dc1a67943a388e4c",
+    "url": "/counter/static/js/main.447a7349.chunk.js"
+  },
+  {
+    "revision": "89b49dece9258b528f00",
+    "url": "/counter/static/js/runtime-main.11b21498.js"
+  }
+]);
